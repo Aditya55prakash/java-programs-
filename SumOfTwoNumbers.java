c4a6a5ec0f3eec5fa36b6dd93dc82118ad1f4240
Aditya55prakash <adityaprakash5383@gmail.com>
2026-09-09 19:51:55 +0530

    Update SumOfTwoNumbers.java
    My Java programs and practice codes for BCA

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -10,7 +10,7 @@ public class SumOfTwoNumbers {
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
 
-        int sum = a + b;
+        int sum = a + b; // Calculate the sum
 
         System.out.println("Sum = " + sum);
     }
